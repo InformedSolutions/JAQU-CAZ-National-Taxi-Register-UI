965311152b46acf1266adb9ef0eabb59f9aea400
Sergii Chub <s.chub@exlabs.pl>
2019-08-22 12:43:10 +0200

    [NTR-373] Add email validation on logon and forgotten password pages

---
+++ app/services/email_validator.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+class EmailValidator < BaseService
+  attr_reader :email
+
+  def initialize(email:)
+    @email = email
+  end
+
+  def call
+    if invalid_format?
+      'Invalid email format'
+    elsif email_too_long?
+      'Email address exceeds the limit of 45 characters'
+    end
+  end
+
+  private
+
+  def email_too_long?
+    email.length > 45
+  end
+
+  def invalid_format?
+    (email =~ URI::MailTo::EMAIL_REGEXP).nil?
+  end
+end
