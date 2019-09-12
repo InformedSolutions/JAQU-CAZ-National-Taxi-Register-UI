66cb4592aaeb78a66d5a51bfbae0b2796314bd81
Sergii Chub <s.chub@exlabs.pl>
2019-09-12 13:18:26 +0200

    [NTR-463] Add models documentation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+##
+# This class is used to authorize a user account.
 class User
   # required by Devise
   include ActiveModel::Validations
@@ -9,19 +11,19 @@ class User
   define_model_callbacks :validation
   devise :remote_authenticatable, :timeoutable
 
+  # Attribute that is being used to authorize a user and use it in csv uploading.
   attr_accessor :email, :username, :aws_status, :aws_session, :sub,
                 :confirmation_code, :hashed_password
 
-  # Latest devise(v4.6.2) tries to initialize this class with values
-  # ignore it for now
+  # Latest devise(v4.7.1) tries to initialize this class with values, ignore it for now.
   def initialize(options = {}); end
 
-  # needed for rendering user forms
+  # Needed for rendering user forms.
   def to_key
     nil
   end
 
-  # needed for displaying user in console
+  # Needed for displaying user in console.
   def serializable_hash(_options = nil)
     {
       email: email,
