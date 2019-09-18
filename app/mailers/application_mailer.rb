63d5aec15c490674b8b84628fb46990be4b337ac
Wojtek Mikusek <miqs1992@gmail.com>
2019-09-18 11:45:21 +0200

    fixup! [NTR-483] Add SES configuration

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+##
+# Base class for mailers. Sets default from value and layout
+#
 class ApplicationMailer < ActionMailer::Base
   default from: ENV.fetch('SES_FROM_EMAIL', 'TaxiandPHVCentralised.Database@defra.gov.uk')
   layout 'mailer'
--- spec/mailers/previews/upload_mailer_preview.rb
+++ spec/mailers/previews/upload_mailer_preview.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
+##
 # Preview all emails at http://localhost:3000/rails/mailers/upload_mailer
+#
 class UploadMailerPreview < ActionMailer::Preview
   def success_upload
     user = User.new
