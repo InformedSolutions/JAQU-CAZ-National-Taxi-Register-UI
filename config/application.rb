b84183b96d6f34822582bd7715f28cbf66394b51
Wojtek Mikusek <miqs1992@gmail.com>
2019-09-19 11:06:48 +0200

    [NTR-484] Add sending success upload email

--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,9 @@ module CsvUploader
     feedback_url_default = 'https://www.surveymonkey.co.uk/r/2T8BX2D'
     config.x.feedback_url = (ENV['FEEDBACK_URL'].presence || feedback_url_default)
 
+    config.time_zone = 'London'
+    config.x.time_format = '%d %B %Y %H:%M:%S %Z'
+
     # https://mattbrictson.com/dynamic-rails-error-pages
     config.exceptions_app = routes
 
