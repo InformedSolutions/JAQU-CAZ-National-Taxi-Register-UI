45c6904d144fc53fda5016004a5632f4d8e7f43e
proctorj <james.proctor@informed.com>
2020-03-05 15:55:38 +0000

    Updates to CAZSM-9 to reflect CAZSM-8: Moved new log formatter to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require 'logstash-logger'
+require 'config/log_format'
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
@@ -105,12 +105,8 @@ Rails.application.configure do
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
-  # Use custom logging formatter with tagged logging so that IP addresses are removed
-  # and request IDs are retained.
-  logger = LogStashLogger.new(type: :stdout)
-
-  # Use tagged logging to include request id on production.
-  config.logger = ActiveSupport::TaggedLogging.new(logger)
+  # Use custom logging formatter so that IP any other PII can be removed.
+  config.log_formatter = FilteredFormatter.new
 
   # Do not dump schema after migrations.
   # config.active_record.dump_schema_after_migration = false
