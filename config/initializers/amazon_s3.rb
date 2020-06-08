8fdd6163591e92a458c0793fd00a17727dfe3fb1
Piotr Kałamarski <pkalamarski@gmail.com>
2020-06-08 17:20:31 +0200

    fix build

--- config/initializers/amazon_s3.rb
+++ config/initializers/amazon_s3.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-credentials = if ENV['S3_AWS_ACCESS_KEY_ID'] || !Rails.env.production?
+credentials = if ENV['S3_AWS_ACCESS_KEY_ID']
                 Aws::Credentials.new(
                   ENV.fetch('S3_AWS_ACCESS_KEY_ID', 'S3_AWS_ACCESS_KEY_ID'),
                   ENV.fetch('S3_AWS_SECRET_ACCESS_KEY', 'S3_AWS_SECRET_ACCESS_KEY')
